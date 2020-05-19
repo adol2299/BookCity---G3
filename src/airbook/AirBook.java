f1beadb717ec1eab5cb944b3b835196993677bf7
adol2299 <adol2299@gmail.com>
2020-05-19 16:15:44 -0500

    Primera versión de GUI

---
+++ src/airbook/AirBook.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package airbook;
+
+import java.io.IOException;
+import javafx.application.*;
+import javafx.fxml.FXMLLoader;
+import java.text.*;
+import java.util.*;
+import javafx.event.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.input.*;
+import javafx.stage.*; 
+import javafx.geometry.*;
+
+/**
+ *
+ * @author Familia Ortega
+ */
+public class AirBook extends Application {
+    
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));         
+            
+        Scene scene = new Scene (root);   
+        scene.getStylesheets().add("");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
