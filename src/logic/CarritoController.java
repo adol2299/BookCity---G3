765494cd5d429169cfb3b58a125698663ba9706f
David Mateus <dmateusb@unal.edu.co>
2020-06-20 17:24:54 -0500

    Arreglo GUI Carrito

---
+++ src/logic/CarritoController.java
@@ -0,0 +1,14 @@
+package logic;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+
+
+public class CarritoController {
+
+    @FXML
+    private void onClicDetalles(MouseEvent event) {
+    }
+    
+}
